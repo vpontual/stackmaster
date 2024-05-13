354e3e3695c058b87a879ec68fdee6d9334f58a0
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-13 14:09:27 -0400

    added categories page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,6 +37,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Choice from './pages/Choice';
+import Categories from './pages/Categories';
 // import Footer from './components/Footer';
 import Footer from './components/Footer';
 
@@ -46,6 +47,7 @@ const App = () => {
       <Header />
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/categories" element={<Categories />} />
         {/* <Route path="/study" element={<Study />} /> */}
         {/* <Route path="/quiz" element={<Quiz />} /> */}
         <Route path="/choice" element={<Choice />} />
--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Choice = () => {
+const Categories = () => {
+  const categories = [
+    { id: 1, name: 'Front End' },
+    { id: 2, name: 'Back End' },
+    { id: 3, name: 'APIs' },
+    { id: 4, name: 'React' },
+    { id: 5, name: 'Databases' },
+    { id: 6, name: 'All' },
+  ];
+
   return (
     <div>
-      <h1>What do you want to do today?</h1>
+      <h1>Select a Category</h1>
       <div>
-        <Link to="/categories">Study</Link>
-        <Link to="/quiz">Take Quiz</Link>
+        {categories.map((category) => (
+          <Link
+            key={category.id}
+            to={`/study/${category.id}`}
+            style={{ display: 'block', margin: '10px 0' }}
+          >
+            {category.name}
+          </Link>
+        ))}
       </div>
     </div>
   );
 };
 
-export default Choice;
+export default Categories;
