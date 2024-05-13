003cfbb50eadb95863bbd2020b69c19c69d35ad8
Nahid Ahmed <npahmed92@gmail.com>
2024-05-13 14:59:21 -0400

    import packages

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-// fill server information
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
