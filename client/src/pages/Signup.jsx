caba51a8781a86f446161e08cbd0d88aefd1c74e
Nahid Ahmed <npahmed92@gmail.com>
2024-05-13 14:09:57 -0400

    useState for form

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Signup () {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  return (
+
+  );
+}
