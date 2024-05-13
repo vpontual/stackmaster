7787edeef5939bbad7f44a4753cbe7255e102ac4
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-13 13:40:21 -0400

    simplified routing to categories or quiz page

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Choice = () => {
-  const handleStudyClick = () => {
-    // Implement navigation to the study category selection page
-    console.log('Study button clicked');
-  };
-
-  const handleQuizClick = () => {
-    // Implement navigation to the quiz page
-    console.log('Quiz button clicked');
-  };
-
   return (
     <div>
       <h1>Welcome to the Learning App</h1>
       <div>
-        <button onClick={handleStudyClick}>Study</button>
-        <button onClick={handleQuizClick}>Take Quiz</button>
+        <Link to="/categories">Study</Link>
+        <Link to="/quiz">Take Quiz</Link>
       </div>
     </div>
   );
