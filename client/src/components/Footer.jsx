7a593a031c286c1c3f1b11366d5e5b31fddfe035
Gary Bright <garybright@rocketmail.com>
2024-05-13 14:41:10 -0400

    new file structure for tailwind. updated footer to use tailwind.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,19 +1,15 @@
-
-
 const Footer = () => {
-  const footerstyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#11111b',
-    padding: '1rem',
-    color: '#f9e2af',
-  }
+  // const footerstyle = {
+  //   display: 'flex',
+  //   justifyContent: 'space-between',
+  //   alignItems: 'center',
+  //   backgroundColor: '#11111b',
+  //   padding: '1rem',
+  //   color: '#f9e2af',
+  // };
   return (
-    <footer style={footerstyle}>  
-        <h4>
-         Get your Stack on! © 2024 StackMaster
-        </h4>
+    <footer className="bg-slate-900 p-4">
+      <h4>Get your Stack on! © 2024 StackMaster</h4>
     </footer>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./client/index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    './index.html',
+   './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
