6dd6cf6dbe578267fb91074431795827009dba7d
Nahid Ahmed <npahmed92@gmail.com>
2024-05-13 22:28:54 -0400

    use useState for form and errors

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+// import { Link } from 'react-router-dom';
+
+// import useMatation from apollo here
+// import mutuation from utils here
+
+// import authentication here
+
+export default function Login(props) {
+  const [formState, setFormState] = useState({
+    username: '',
+    password: '',
+  })
+
+  const [errors, setErrors] = useState({
+    username: '',
+    password: '',
+  })
+
+  return <div>Hello</div>
+}
