a16fc04567472afb226a0d9d57c7861058cda30e
Stewpra <wilbstewart7@gmail.com>
2024-05-14 14:39:48 -0400

    route to study catagory name

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,32 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Categories = () => {
   const categories = [
-    { id: 1, name: 'Front End' },
+    { id: 1, name: 'JavaScript' },
     { id: 2, name: 'Back End' },
     { id: 3, name: 'APIs' },
     { id: 4, name: 'React' },
     { id: 5, name: 'Databases' },
     { id: 6, name: 'All' },
-  ];
+  ]
 
   return (
     <div>
       <h1>Select a Category</h1>
       <div>
-        {categories.map((category) => (
-          <Link
-            key={category.id}
-            to={`/study/${category.id}`}
-            style={{ display: 'block', margin: '10px 0' }}
-          >
+        {categories.map(category => (
+          <Link key={category.id} to={`/study/${category.name}`} style={{ display: 'block', margin: '10px 0' }}>
             {category.name}
           </Link>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Categories;
+export default Categories
