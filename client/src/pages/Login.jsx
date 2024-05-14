fea1266121eb71148a32efd687663c26ce4bfac3
Nahid Ahmed <npahmed92@gmail.com>
2024-05-14 19:55:51 -0400

    add imports

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,75 +1,77 @@
-import { useState } from 'react'
-// import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-// import useMatation from apollo here
-// import mutuation from utils here
-
+// import useMutation from apollo here
+import { useMutation } from '@apollo/client';
+// import mutation from utils here
+import { LOGIN_USER } from '../utils/mutations';
 // import authentication here
+import Auth from '../utils/auth';
 
 export default function Login(props) {
   const [formState, setFormState] = useState({
     username: '',
     password: '',
-  })
+  });
 
   const [errors, setErrors] = useState({
     username: '',
     password: '',
-  })
+  });
 
   // use mutation here to add user
 
-  const handleChange = event => {
-    const { name, value } = event.target
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-    setFormState({ ...formState, [name]: value })
+    setFormState({ ...formState, [name]: value });
 
     // reset error when user types
-    setErrors({ ...errors, [name]: '' })
-  }
+    setErrors({ ...errors, [name]: '' });
+  };
 
-  const handleFormSubmit = event => {
-    event.preventDefault()
-    console.log(formState)
-    let valid = true
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+    let valid = true;
 
     // validate username field
     if (!formState.username.trim()) {
-      setErrors(prevErrors => ({
+      setErrors((prevErrors) => ({
         ...prevErrors,
         username: 'Username is required',
-      }))
-      valid = false
+      }));
+      valid = false;
     }
 
     // validate password format
     if (!formState.password.trim()) {
       // validate password field
-      setErrors(prevErrors => ({
+      setErrors((prevErrors) => ({
         ...prevErrors,
         password: 'Password is required',
-      }))
-      valid = false
+      }));
+      valid = false;
     }
 
     // add try catch error for add user and authenticate login
 
     // valid form
     if (valid) {
-      console.log('Form submitted:', formState)
+      console.log('Form submitted:', formState);
 
       // reset form after login
       setFormState({
         username: '',
         password: '',
-      })
+      });
     }
-  }
+  };
 
   return (
     <main className="flex justify-center mb-4">
       <div className="w-full lg:w-10/12">
-        <div className="bg-white shadow-md">
+        <div>
           <h4 className="bg-dark text-white py-2 px-4">Login</h4>
           <div className="p-4">
             {/* add ternary operator here with data and then load choices with Link */}
@@ -94,7 +96,7 @@ export default function Login(props) {
                 onChange={handleChange}
               />
               {errors.password && <div className="text-red-500">{errors.password}</div>}
-              <button className="w-full py-2 bg-blue-500 text-white rounded-lg cursor-pointer mt-4" type="submit">
+              <button className="w-full py-2 text-white rounded-lg cursor-pointer mt-4" type="submit">
                 Submit
               </button>
             </form>
@@ -103,5 +105,5 @@ export default function Login(props) {
         </div>
       </div>
     </main>
-  )
+  );
 }
