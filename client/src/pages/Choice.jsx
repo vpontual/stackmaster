6a87ee856d21b1ac036b771b242492c474808f74
Gary Bright <garybright@rocketmail.com>
2024-05-14 15:53:06 -0400

    matched the buttons to the header colors

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Choice = () => {
   return (
     <div>
-      <h1 className="flex justify-center pb-10">What do you want to do today?</h1>
+      <h1 className="flex justify-center pb-10 text-header">What do you want to do today?</h1>
       <div className="flex justify-evenly pt-20 ">
-        <div className="flex justify-center items-center w-80 h-40 bg-slate-400 text-5xl">
+        <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl text-red-500 ">
         <Link to="/categories">Study</Link>
         </div>
-        <div className="flex justify-center items-center w-80 h-40 bg-slate-400 text-5xl">
+        <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl">
         <Link to="/quiz">Take Quiz</Link>
         </div>
       </div>
