aae21524af179674f7b78a49394a0969fd311fa4
Gary Bright <garybright@rocketmail.com>
2024-05-14 15:46:10 -0400

    added color and spacing to buttons

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -5,11 +5,11 @@ const Choice = () => {
   return (
     <div>
       <h1 className="flex justify-center pb-10">What do you want to do today?</h1>
-      <div className="flex justify-center justify-evenly pt-20">
-        <div className="">
+      <div className="flex justify-evenly pt-20 ">
+        <div className="flex justify-center items-center w-80 h-40 bg-slate-400 text-5xl">
         <Link to="/categories">Study</Link>
         </div>
-        <div>
+        <div className="flex justify-center items-center w-80 h-40 bg-slate-400 text-5xl">
         <Link to="/quiz">Take Quiz</Link>
         </div>
       </div>
