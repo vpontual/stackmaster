3ae0041be0b0176eb55b6f9b0ff52da84c90804a
Nahid Ahmed <npahmed92@gmail.com>
2024-05-14 18:20:51 -0400

    change model to Question instead of Questions

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,28 +1,28 @@
-const db = require('../config/connection')
-const { User, Questions } = require('../models')
-const userSeeds = require('./userSeeds.json')
-const questionSeeds = require('./questionSeeds.json')
-const cleanDB = require('./cleanDB')
+const db = require('../config/connection');
+const { User, Question } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const questionSeeds = require('./questionSeeds.json');
+const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
-    await cleanDB('User', 'users')
-    await User.create(userSeeds)
+    await cleanDB('User', 'users');
+    await User.create(userSeeds);
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
 
-  console.log('all done!')
-  process.exit(0)
-})
+  console.log('all done!');
+  process.exit(0);
+});
 
 db.once('open', async () => {
   try {
-    await cleanDB('Questions')
-    await Questions.create(questionSeeds)
+    await cleanDB('Question');
+    await Question.create(questionSeeds);
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-})
+});
