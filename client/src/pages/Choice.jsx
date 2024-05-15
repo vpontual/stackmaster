890f6896767437b701315d09725e2827fac8d99a
Nahid Ahmed <npahmed92@gmail.com>
2024-05-14 23:26:20 -0400

    grab username and display on choice page

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Auth from '../utils/auth';
+
 const Choice = () => {
   return (
     <div>
-      <h1>What do you want to do today?</h1>
+      <h1>
+        What do you want to do today <span className="text-header uppercase">{Auth.getProfile().data.username}</span> ?
+      </h1>
       <div>
         <Link to="/categories">Study</Link>
         <Link to="/quiz">Take Quiz</Link>
