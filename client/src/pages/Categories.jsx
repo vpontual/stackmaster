df16638231878c07cee621465b75155d62b60cab
Gary Bright <garybright@rocketmail.com>
2024-05-15 11:03:14 -0400

    updated categories to recieve indiviual style to links

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -14,15 +14,21 @@ const Categories = () => {
   return (
     <div>
       <h1 className="flex justify-center p-20 text-header">Select a Category</h1>
-      <div className="flex justify-center justify-items-center w-90 h-40  bg-banner text-4xl grid gap-4 grid-cols-3">
-        {categories.map(category => (
-          <Link className="text-inherit hover:text-green-800 hover:animate-jump-in"key={category.id} to={`/study/${category.name}`} style={{ display: 'block', margin: '10px 0' }}>
-            {category.name}
-          </Link>
+      <div className="flex justify-center justify-items-center w-90 h-40 text-4xl grid gap-4 grid-cols-3">
+        {categories.map((category) => (
+          <div className="bg-banner p-5 rounded-xl w-60 text-center" key={category.id}>
+            <Link
+              className="text-inherit hover:text-header hover:animate-jump-in"
+              key={category.id}
+              to={`/study/${category.name}`}
+              style={{ display: 'block', margin: '10px 0' }}>
+              {category.name}
+            </Link>
+          </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default Categories
