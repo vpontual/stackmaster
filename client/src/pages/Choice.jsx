49c84bad5ff560e13ae2bd8533021391978cd388
Gary Bright <garybright@rocketmail.com>
2024-05-14 23:55:36 -0400

    added style to the links

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Choice = () => {
   return (
     <div>
-      <h1 className="flex justify-center pb-10 text-header">What do you want to do today?</h1>
-      <div className="flex justify-evenly pt-20 ">
-        <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl text-red-500 ">
-        <Link to="/categories">Study</Link>
+      <h1 className="flex justify-center p-20 text-header">What do you want to do today?</h1>
+      <div className="flex justify-center space-x-20 pt-20 ">
+        <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl ">
+        <Link to="/categories"className="text-green-600 hover:text-green-800">Study</Link>
         </div>
         <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl">
         <Link to="/quiz">Take Quiz</Link>
