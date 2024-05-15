4bf4daa79d8debc31099edf1502eb66d59975058
Gary Bright <garybright@rocketmail.com>
2024-05-15 00:33:58 -0400

    added tailwind animate to package.json, added animation to hover over choice links

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -7,10 +7,10 @@ const Choice = () => {
       <h1 className="flex justify-center p-20 text-header">What do you want to do today?</h1>
       <div className="flex justify-center space-x-20 pt-20 ">
         <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl ">
-        <Link to="/categories"className="text-green-600 hover:text-green-800">Study</Link>
+        <Link to="/categories"className="text-inherit hover:text-green-800 hover:animate-jump-in">Study</Link>
         </div>
         <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl">
-        <Link to="/quiz">Take Quiz</Link>
+        <Link to="/quiz"className="text-inherit hover:text-green-800 hover:animate-jump-in">Take Quiz</Link>
         </div>
       </div>
     </div>
