d6cd2f1cd6c6207e2c3006de813c0fdbbb5b32bc
Gary Bright <garybright@rocketmail.com>
2024-05-15 11:48:03 -0400

    adjusted color to work with background

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -9,10 +9,10 @@ const Choice = () => {
       <h1 className="flex justify-center p-20 text-header">What would you like to do today?</h1>
       <div className="flex justify-center space-x-20 pt-20 ">
         <div className="flex justify-center items-center w-80 h-40 bg-index rounded-xl text-5xl ">
-        <Link to="/categories"className="text-inherit hover:text-green-800 hover:animate-jump-in">Study</Link>
+        <Link to="/categories"className="text-banner hover:text- hover:animate-jump-in">Study</Link>
         </div>
-        <div className="flex justify-center items-center w-80 h-40 bg-banner rounded-xl text-5xl">
-        <Link to="/quiz"className="text-inherit hover:text-green-800 hover:animate-jump-in">Take Quiz</Link>
+        <div className="flex justify-center items-center w-80 h-40 bg-index rounded-xl text-5xl">
+        <Link to="/quiz"className="text-banner hover:text-green-800 hover:animate-jump-in">Take Quiz</Link>
         </div>
       </div>
     </div>
