a181b5f1b121d51dc250c01e406411adbf369c3b
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-15 11:17:49 -0400

    added sample

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -84,4 +84,25 @@ function App() {
   );
 }
 
+const SAMPLE_CARDS = [
+  {
+    id: 1,
+    question: 'What is 3 + 3 ?',
+    answer: '4',
+    options: ['2', '3', '5', '4'],
+  },
+  {
+    id: 2,
+    question: 'What is four + four ?',
+    answer: '8',
+    options: ['2', '3', '8', '4'],
+  },
+  {
+    id: 3,
+    question: 'What is six * six ?',
+    answer: '36',
+    options: ['2', '36', '5', '4'],
+  },
+];
+
 export default App;
