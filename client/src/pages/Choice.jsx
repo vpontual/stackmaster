0c2252fdfcfd8ca7e4acc86814591a430f4dfc6f
Gary Bright <garybright@rocketmail.com>
2024-05-15 00:45:02 -0400

    reworded main text on choice page

--- client/src/pages/Choice.jsx
+++ client/src/pages/Choice.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Choice = () => {
   return (
     <div>
-      <h1 className="flex justify-center p-20 text-header">What do you want to do today?</h1>
+      <h1 className="flex justify-center p-20 text-header">What would you like to do today?</h1>
       <div className="flex justify-center space-x-20 pt-20 ">
         <div className="flex justify-center items-center w-80 h-40 bg-banner text-5xl ">
         <Link to="/categories"className="text-inherit hover:text-green-800 hover:animate-jump-in">Study</Link>
