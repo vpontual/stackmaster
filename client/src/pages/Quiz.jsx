9eab05c6f3fea5e19b8e55ac061e1cd11be6eae5
Nahid Ahmed <npahmed92@gmail.com>
2024-05-15 23:03:07 -0400

    add quiz work in progress page

---
+++ client/src/pages/Quiz.jsx
@@ -0,0 +1,12 @@
+import BugLogo from '../assets/bug';
+
+export default function Quiz() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <BugLogo className="text-red-500 mx-2" aria-label="Github logo" height="300" width="300" />
+      <h1 className="text-center text-red-500 mb-4">
+        You haven't reached the peak of the stack yet. Keep climbing, and you'll debug it eventually!
+      </h1>
+    </div>
+  );
+}
