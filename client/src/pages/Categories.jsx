148a6b49dace3dd17cad96690cc124f03e6ac8cb
Nahid Ahmed <npahmed92@gmail.com>
2024-05-15 23:44:10 -0400

    remove unused import, style page, remove space for study/:name

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
   const categories = [
@@ -9,18 +8,20 @@ const Categories = () => {
     { id: 4, name: 'React' },
     { id: 5, name: 'Databases' },
     { id: 6, name: 'All' },
-  ]
+  ];
 
   return (
     <div>
-      <h1 className="flex justify-center p-20 text-header">Select a Category</h1>
+      <h1 className="flex justify-center p-10 text-header">Select a Category</h1>
       <div className="flex justify-center justify-items-center w-3/5 h-40 ml-56 text-4xl grid gap-y-10 grid-cols-2">
         {categories.map((category) => (
-          <div className="flex justify-center items-center bg-index p-5 rounded h-40 w-80 text-center" key={category.id}>
+          <div
+            className="flex justify-center items-center bg-index p-5 rounded h-40 w-80 text-center"
+            key={category.id}>
             <Link
               className="text-banner hover:text-slate-400 hover:animate-bounce"
               key={category.id}
-              to={`/study/${category.name}`}
+              to={`/study/${category.name}`.replace(/\s/g, '')}
               style={{ display: 'block', margin: '10px 0' }}>
               {category.name}
             </Link>
@@ -29,6 +30,6 @@ const Categories = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Categories
+export default Categories;
