e4e294177380c124ac23c61ad085acf3f72e734b
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-16 12:06:40 -0400

    improved stripe form and thank you message

--- client/src/components/DonationForm.jsx
+++ client/src/components/DonationForm.jsx
@@ -30,7 +30,7 @@ const DonationForm = ({ handleCloseForm }) => {
       setPaymentSuccess(true);
       setTimeout(() => {
         handleCloseForm();
-      }, 3000);
+      }, 4000);
     }
   };
 
@@ -38,22 +38,25 @@ const DonationForm = ({ handleCloseForm }) => {
     <div className="donation-form">
       {paymentSuccess ? (
         <div className="text-center">
-          <h2 className="text-2xl font-bold mb-4">Thank you for your donation!</h2>
-          <p>Your support means a lot to us.</p>
+          <h2 className="text-2xl font-bold mb-4 text-gray-700">Thank you for your donation!</h2>
+          <p className="text-gray-700">Your support means a lot to us 😊</p>
         </div>
       ) : (
-        <form onSubmit={handleSubmit} className="bg-white rounded-lg p-8 w-96">
-          <div className="mb-4">
-            <CardElement className="border p-4 rounded" />
-          </div>
-          {paymentError && <div className="text-red-500 mb-4">{paymentError}</div>}
-          <button
-            type="submit"
-            disabled={!stripe}
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full">
-            Donate $5
-          </button>
-        </form>
+        <div>
+          <p className="font-bold mb-4 text-center  text-gray-700">All donations help us keep the site up for you.</p>
+          <form onSubmit={handleSubmit} className="bg-white rounded-lg p-8 w-96">
+            <div className="mb-4">
+              <CardElement className="border p-4 rounded" />
+            </div>
+            {paymentError && <div className="text-red-500 mb-4">{paymentError}</div>}
+            <button
+              type="submit"
+              disabled={!stripe}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full">
+              Donate $5
+            </button>
+          </form>
+        </div>
       )}
     </div>
   );
