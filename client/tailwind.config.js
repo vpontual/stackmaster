4d1097b71ea0f7ae8b8d4953684a62fd0319adbd
Gary Bright <garybright@rocketmail.com>
2024-05-16 00:48:40 -0400

    added comments for choice and categorie style changes.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         banner: '#11111b',
         header: '#f9e2af',
       },
+      //index card svg added for background of link selection
       backgroundImage: {
         'index': "url(./assets/card.svg)"
       }
