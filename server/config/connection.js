559dd0fc3aa4743cd77b55068494b2aa67d4a1d1
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-16 13:27:53 -0400

    updated production start script

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
